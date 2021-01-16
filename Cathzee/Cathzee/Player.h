8d598710bac88602fd996b43b23f9dd63459880d
Piotr Smilgin <waveoffire98@gmail.com>
2021-01-16 16:07:27 +0100

    More functions and private variables

--- Cathzee/Cathzee/Player.h
+++ Cathzee/Cathzee/Player.h
@@ -3,22 +3,35 @@
 class Player
 {
 public:
-	int score = 0; //prawo dostêpu: publiczne
-	int ones; //prawo dostêpu: publiczne
-	int twos;
-	int threes;
-	int fours;
-	int fives;
-	int sixs;
-	int RollDice();
-	void ReReollDice();
-	void HoldDice();
-	void DisplayCurDice();
-	void TakeTurn();
-
+    Player() { playerScore = 0; topScore = 0; aces = -1; twos = -1; threes = -1; fours = -1; fives = -1; sixes = -1; bonus = -1; threeKind = -1; fourKind = -1; fullHouse = -1; smStraight = -1; lrgStraight = -1; yahtzee = -1; chance = -1; bonus = -1; slotsFilled = 0; }
+    int rollDice();
+    void rollFiveDice();
+    void showCurrentDice();
+    void holdDice();
+    void takeTurn();
+    void reRoll();
+    void selectScoreCard();
+    void displayPlayerCard();
+    void endGame();
+    int displayPlayerScore();
 private:
-	int abc; //prawo dostêpu: prytatne
-	char znak; //prawo dostêpu: prytatne
-	std::string napis; //prawo dostêpu: prytatne
+    int diceArray[5];
+    int playerScore;
+    int topScore;
+    int aces;
+    int twos;
+    int threes;
+    int fours;
+    int fives;
+    int sixes;
+    int bonus;
+    int threeKind;
+    int fourKind;
+    int fullHouse;
+    int smStraight;
+    int lrgStraight;
+    int yahtzee;
+    int chance;
+    int slotsFilled;
 };
 
