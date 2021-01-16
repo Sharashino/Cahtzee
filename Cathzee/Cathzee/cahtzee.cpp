25a1e99a7a149ac317abec93242b39782dc32509
Sharashino <sharashino@icloud.com>
2021-01-16 15:37:36 +0100

    Add cahtzee.cpp

---
+++ Cathzee/Cathzee/cahtzee.cpp
@@ -0,0 +1,42 @@
+#include "Player.h"
+
+int Player::RollDice()
+{
+	int result = (rand() % 6) + 1;
+	
+	return result;
+}
+
+void Player::ReRollDice()
+{
+
+}
+
+void Player::HoldDice()
+{
+
+}
+
+void Player::DisplayCurDice()
+{
+
+}
+
+void Player::TakeTurn()
+{
+
+}
+
+void Player::DisplayPlayerScore()
+{
+
+}
+
+void Player::EndGame()
+{
+
+}
+
+
+
+
